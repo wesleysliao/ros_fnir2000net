5603eef63b6d833c8360d4f4baed3ad97de168b8
wesliao <wesliao@iu.edu>
2019-07-29 11:26:58 -0400

    Corrected data format and added basic ROS publishing

--- src/fnir2000_node.py
+++ src/fnir2000_node.py
@@ -3,8 +3,9 @@
 import socket
 import struct 
 
-#TCP_IP = '149.161.238.148'
-#TCP_PORT = 6350
+import rospy
+from ros_fnir2000net.msg import fNIROptode, fNIRArray
+from std_msgs.msg import Header
 
 TCP_IP = '192.168.56.101'
 TCP_PORT = 6343
@@ -17,23 +18,34 @@ s.connect((TCP_IP, TCP_PORT))
 #send 'u' to COBI studio to complete handshake 
 s.sendall(b'u')
 
-while(True):
-    data = s.recv(BUFFER_SIZE)
+def fnir_sender():
+    pub = rospy.Publisher('fnir', fNIROptode, queue_size = 10)
+    rospy.init_node('fnir2000_node', anonymous = True)
+    fnirmsg = fNIROptode(raw_730nm = raw_730nm,
+                         raw_ambient = raw_ambient,
+                         raw_850nm = raw_850nm)
+    pub.publish(fnirmsg)
+    
+while True:
     try:
-        data_unpacked = struct.unpack_from('<H54i', data)     
+        data = s.recv(BUFFER_SIZE)
+        print data
+        data_unpacked = struct.unpack_from('<B54f', data)     
         
+        optodelist = []
         for optode, index_offset in enumerate(range(1, 54, 3), start=1):
             raw_730nm = data_unpacked[index_offset]
+            #code0 = data_unpacked[index_offset+1] 
             raw_ambient = data_unpacked[index_offset+1]
+            #code1 = data_unpacked[index_offset+3] 
             raw_850nm = data_unpacked[index_offset+2]
+            #code2 = data_unpacked[index_offset+5]
             
-            print( optode, raw_730nm, raw_ambient, raw_850nm)
-        
+            fnir_data = [optode, raw_730nm, raw_ambient, raw_850nm]
+            print(fnir_data)
+            fnir_sender()
+ 
     except Exception as error:
         print error
-        
-        
-        data_unpacked = struct.unpack_from('B', data)
-        print data_unpacked
-
+               
 s.close()
